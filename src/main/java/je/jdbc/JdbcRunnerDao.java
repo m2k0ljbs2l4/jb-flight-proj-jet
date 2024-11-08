582d08bccc608917317567798db5c4d5c2fb1d3c
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-08 21:09:37 +0500

    add JdbcRunnerDao

---
+++ src/main/java/je/jdbc/JdbcRunnerDao.java
@@ -0,0 +1,24 @@
+package je.jdbc;
+
+import je.jdbc.dao.TicketDao;
+import je.jdbc.entity.Ticket;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+public class JdbcRunnerDao {
+
+    public static void main(String[] args) throws SQLException {
+        TicketDao ticketDao = TicketDao.getInstance();
+        Ticket ticket = new Ticket(null, null, null, null, null, null);
+        ticket.setPassangerNo("adf123");
+        ticket.setPassangerName("Andrei");
+        ticket.setFlightId(4L);
+        ticket.setSeatNo("5B");
+        ticket.setCost(BigDecimal.TEN);
+
+        System.out.println(ticketDao.save(ticket));
+        System.out.println(ticketDao.delete(56L));
+
+    }
+}
