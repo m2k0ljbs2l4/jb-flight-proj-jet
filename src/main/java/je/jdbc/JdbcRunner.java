0ef3f45ec2a0eac3a5559950f77abb5b66b07faa
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-08 16:38:59 +0500

    execute example

--- src/main/java/je/jdbc/JdbcRunner.java
+++ src/main/java/je/jdbc/JdbcRunner.java
@@ -4,15 +4,41 @@ import je.jdbc.utils.ConnectionManager;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
+//        String sql = """
+//                    CREATE TABLE info (
+//                    id SERIAL PRIMARY KEY,
+//                    data varchar(255)
+//                );
+//                """;
 
+//        String sql = """
+//                    INSERT INTO info (data)
+//                    VALUES ('TEXT 1'),
+//                    ('TEXT 2'),
+//                    ('TEXT 3'),
+//                    ('TEXT 4'),
+//                    ('TEXT 5');
+//                """;
 
+//        String sql = """
+//        DELETE FROM info;
+//        """;
+//        String sql = """
+//        DROP TABLE info;
+//        """;
 
-        try (Connection connection = ConnectionManager.open()) {
-            System.out.println(connection.getTransactionIsolation());;
+        String sql = """
+        DELETE FROM info;
+        """;
+
+        try (Connection connection = ConnectionManager.open();
+             Statement statement = connection.createStatement()) {
+            System.out.println(statement.execute(sql));
         }
     }
 
