92d2079af4aff69c3a11763b031cc2f7e036640b
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-09 16:20:08 +0500

    add class CookiesServlet

---
+++ src/main/java/je/jdbc/servlet/CookieServlet.java
@@ -0,0 +1,31 @@
+package je.jdbc.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet("/cookies")
+public class CookieServlet extends HttpServlet {
+
+    private static final String UNIQ_USER = "userId";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+        if (cookies == null ||
+            Arrays.stream(cookies).filter(cookie ->
+                UNIQ_USER.equals(cookie.getName())).findFirst().isEmpty()) {
+            Cookie cookie = new Cookie(UNIQ_USER, "1");
+            cookie.setMaxAge(60 * 60);
+            cookie.setPath("/cookies");
+            resp.addCookie(cookie);
+        }
+    }
+}
+
